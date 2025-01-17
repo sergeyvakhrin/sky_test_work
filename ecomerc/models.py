7fe49de8d1e5363b8e5d31045fc93b9130dcb73b
sergeyvakhrin <parlamentsv@mail.ru>
2025-01-17 16:49:08 +0300

    fix models

--- ecomerc/models.py
+++ ecomerc/models.py
@@ -53,6 +53,9 @@ class Product(models.Model):
     """ Модель для таблицы Продуктов """
     name = models.CharField(max_length=255, verbose_name='Название', help_text='Укажите название продукта', )
     model_product = models.CharField(max_length=255, verbose_name='Модель', help_text='Укажите модель продукта', **NULLABLE)
+    description = models.TextField(verbose_name='Описание', help_text='Добавьте описание продукта', **NULLABLE)
+    price = models.FloatField(verbose_name='Цена', help_text='Укажите стоимость', default=0)
+    is_published = models.BooleanField(verbose_name='В продаже', help_text='Отметьте если в продаже', default=True)
     release_date = models.DateField(verbose_name='Дата выхода', help_text='Укажите дату выхода на рынок', **NULLABLE)
 
     class Meta:
